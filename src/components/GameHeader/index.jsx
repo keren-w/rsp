8bb130b8769314e3e5630df5ce5097a377a81a0d
Keren Wigelman <keren.w@gmail.com>
2024-05-06 04:02:49 -0400

    ui updates, transitions, css vars

--- src/components/GameHeader/index.jsx
+++ src/components/GameHeader/index.jsx
@@ -1,26 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import styles from './GameHeader.module.css';
 import { GameContext } from '../../common/gameContext';
 const GameHeader = () => {
     const { scores, round } = useContext(GameContext);
     return (
         <div className={styles.resultBoard}>
-            <div className={styles.scoreDisplay}>
-                <div>User Score</div>
-                <span className={styles.score}>{scores.user}</span>
-            </div>
 
+            <ScoreDisplay participant="User" score={scores.user} />
             <div className={styles.title}>
                 <h1 className={styles.gameTitle}>Rock Paper Scissors</h1>
                 <h2 className={styles.roundTitle}>Round {round}</h2>
             </div>
 
-            <div className={styles.scoreDisplay}>
-                <div>Computer Score</div>
-                <span className={styles.score}>{scores.computer}</span>
-            </div>
+            <ScoreDisplay participant="Computer" score={scores.computer} />
         </div>
     )
 };
 
 export default GameHeader;
+
+const ScoreDisplay = ({participant, score}) => {
+
+    const [showUpdate, setShowUpdate] = useState(false);
+    useEffect(() => {
+        setShowUpdate(true);
+        setTimeout(() => setShowUpdate(false), 750);
+    }, [score]);
+    
+    return (
+        <div className={styles.scoreDisplay}>
+            <div>{participant} Score</div>
+            <span className={`${styles.score} ${showUpdate ? styles.updateScore : ''}`}>{showUpdate ? '+1' : score}</span>
+        </div>
+    )
+}
--- src/components/SelectTool/index.jsx
+++ src/components/SelectTool/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './SelectTool.module.css';
 import {TOOLS} from '../Game/utils';
 import Tool from '../ToolButton';
-import {useState} from 'react'
 
 const SelectTool = ({userSelection, setUserSelection, disabled}) => {
     return (
